1937a4d32d0760e94584151384330f355a556e61
yuvraj-2503 <singh.yuvraj1047@gmail.com>
2025-07-01 00:22:19 +0530

    fix

--- Package.swift
+++ Package.swift
@@ -23,7 +23,10 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .target(name: "filestore"),
+        .target(name: "filestore",
+                dependencies: [
+                    .product(name: "util", package: "util-swift-sdk")
+                ]),
         .target(
             name: "user-manager",
             dependencies: [

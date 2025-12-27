8ac744a60a0ade4488d33872c71377f8885b675f
yuvraj-2503 <singh.yuvraj1047@gmail.com>
2025-12-27 19:09:06 +0530

    result construcctor added

--- Sources/user-manager/common/Result.swift
+++ Sources/user-manager/common/Result.swift
@@ -8,5 +8,10 @@
 public struct Result: Codable {
     let code: String
     let message: String
+    
+    public init(code: String, message: String) {
+        self.code = code
+        self.message = message
+    }
 }
 
